cf2d40e9af6222579b9d0baeabafe3beda0e222f
VictorSR117 <victorvidas@hotmail.com>
2023-03-02 14:59:36 -0300

    Modulos em JS

---
+++ Trabalhando com Módulos em JS/funcoes.mjs
@@ -0,0 +1,5 @@
+const mostraIdade = (nome, idade) => `a Idade de ${nome} é ${idade}`;
+const mostraCidade = (nome, cidade) => `a Cidade de ${nome} é ${cidade}`;
+const mostraHobbie = (nome, hobbie) => `o Hobbie de ${nome} é ${hobbie}`;
+
+export {mostraIdade, mostraCidade, mostraHobbie}
---
+++ Trabalhando com Módulos em JS/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobbie } from "./funcoes.mjs";
+
+console.log(mostraIdade("Victor", 20));
+console.log(mostraCidade("Victor", "São Paulo"));
+console.log(mostraHobbie("Victor", "Jogar"));
