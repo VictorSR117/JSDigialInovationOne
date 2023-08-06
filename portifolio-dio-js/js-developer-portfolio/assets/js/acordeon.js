b0f12a600f3adb15ba77c535a181ddad522a67a2
VictorSR117 <67758133+VictorSR117@users.noreply.github.com>
2023-08-06 15:57:34 -0300

    test

--- portifolio-dio-js/js-developer-portfolio/assets/js/acordeon.js
+++
@@ -1,9 +0,0 @@
-
-const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
-
-acordeonTriggers.forEach((trigger) => {
-    trigger.addEventListener('click', (e) => {
-        const acordeon = trigger.parentElement;
-        acordeon.classList.toggle('open');
-    })
-})
