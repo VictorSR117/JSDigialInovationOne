019488a89cf63a894bfca3f6c19e8d0935aed053
VictorSR117 <victorvidas@hotmail.com>
2023-08-06 18:30:49 -0300

    modelo de portifolio temporario

--- js-developer-portfolio/assets/js/acordeon.js
+++ js-developer-portfolio/assets/js/acordeon.js
@@ -3,7 +3,7 @@ const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
 
 acordeonTriggers.forEach((trigger) => {
     trigger.addEventListener('click', (e) => {
-        const acordeon = trigger.parentElement;
+        const acordeon = trigger.parentElement
         acordeon.classList.toggle('open');
     })
 })
